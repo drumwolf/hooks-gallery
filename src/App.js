c2c60c75a6af2453ee1871cedfc135793ce0b906
John Lee <drumwolf@gmail.com>
2019-11-07 12:56:50 -0800

    create basic counter with react hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const AppBtn = props => {
+  return  <div className="App-count">{props.count}</div>;
+}
+
+const App = () => {
+  let [count, setCount] = useState(0);
+  let [max, setMax] = useState(7);
+
+  const incrementCount = (inc) => {
+    setCount( (count + inc + max) % max );
+  };
+
+  return (
+    <div className="App">
+      <AppBtn count={count} />
+      <button className="App-btn" onClick={ incrementCount.bind(this, -1) }> - </button>
+      <button className="App-btn" onClick={ incrementCount.bind(this, 1) }> + </button>
+    </div>
+  );
 }
 
 export default App;
