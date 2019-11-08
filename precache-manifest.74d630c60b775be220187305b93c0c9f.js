067b84e9a777e47c3d12053b2130163423a41162
John Lee <drumwolf@gmail.com>
2019-11-07 22:06:28 -0800

    Updates

---
+++ precache-manifest.74d630c60b775be220187305b93c0c9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a687d0e30247a5c118aea91f8ba36a",
+    "url": "/hooks-gallery/index.html"
+  },
+  {
+    "revision": "c3683295eb6a050ce20d",
+    "url": "/hooks-gallery/static/css/main.ab038cb4.chunk.css"
+  },
+  {
+    "revision": "9050cc1f10d11aa57cfb",
+    "url": "/hooks-gallery/static/js/2.86b5df3a.chunk.js"
+  },
+  {
+    "revision": "c3683295eb6a050ce20d",
+    "url": "/hooks-gallery/static/js/main.2a9f4219.chunk.js"
+  },
+  {
+    "revision": "53f7bb7ec62c5eb61268",
+    "url": "/hooks-gallery/static/js/runtime-main.adfeb84b.js"
+  }
+]);
